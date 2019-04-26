eeeba9da3b611e4d5a2f2070f3e417bd20eafb48
darkbear88 <darkbear88@gmail.com>
2019-04-26 09:28:04 +0900

    Revert "총 발사"
    This reverts commit 92b30e8218bfa041b90d74f3717d362965295b7a.

--- TeamProject_Server2.cpp
+++ TeamProject_Server2.cpp
@@ -25,14 +25,9 @@ enum eMSG {
 	em_USER_ZKEY,
 	em_USER_XKEY,
 	em_USER_CKEY,
-	em_USER_VKEY,
-	em_USER_RKEY,
+	em_USER_VKEY,	
 	em_USER_GAMEDEATH,
-	em_USER_RESOURCE,
-	em_USER_STATUS,
-	em_OBJECT_TREE,
-	em_OBJECT_STONE,
-	em_OBJECT_BULLET,
+
 };
 struct sFlag {
 	char flag;
@@ -88,15 +83,22 @@ typedef struct {//buffer info
 
 unsigned WINAPI EchoThreadMain(LPVOID CompletionPortIO);
 void ErrorHandling(const char *message);
-int a;
+
 list<sLogin> loginlist;
 list<sMatching> matchinglist;
 HANDLE hMutex;
-list<sMatching>::iterator miter;
 void vMatching(SOCKET sock);
 void vTeam(SOCKET sock);
-void vMessage(SOCKET sock, char* buffer);
+void vPosition(SOCKET sock, char* buffer);
+void vRayshot(SOCKET sock, char* buffer);
+void vCamera(SOCKET sock, char* buffer);
+void vFkey(SOCKET sock, char* buffer);
+void vZkey(SOCKET sock, char* buffer);
+void vXkey(SOCKET sock, char* buffer);
+void vVkey(SOCKET sock, char* buffer);
+void vCkey(SOCKET sock, char* buffer);
 void vLogin(SOCKET sock, char* buffer);
+void vGamedeath(SOCKET sock, char* buffer);
 
 int main()
 {
@@ -113,7 +115,7 @@ int main()
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		ErrorHandling("WSAStartup() error!");
 
-	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);//이밴트 큐 형태로 만드는것.(LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);//이밴트 큐 형태로 만드는것.
 	GetSystemInfo(&sysInfo);//컴퓨터 기본 정보 받기
 	for (i = 0; i < sysInfo.dwNumberOfProcessors; i++)//코어 갯수 만큼 쓰레드 생성.
 		_beginthreadex(NULL, 0, EchoThreadMain, (LPVOID)hComPort, 0, NULL);
@@ -139,7 +141,7 @@ int main()
 
 		CreateIoCompletionPort((HANDLE)hClntSock, hComPort, (DWORD)handleInfo, 0);
 
-		ioInfo = new PER_IO_DATA;
+		ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 		memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 		ioInfo->wsabuf.len = BUF_SIZE;
 		ioInfo->wsabuf.buf = ioInfo->buffer;
@@ -164,7 +166,7 @@ unsigned WINAPI EchoThreadMain(LPVOID pComPort) {
 
 		if (ioInfo->rwMode == READ) {
 			puts("message received!");
-			if (bytesTrans <= 0)//EOF 전송 시
+			if (bytesTrans == 0)//EOF 전송 시
 			{
 				//로그인 관련
 				list<sLogin>::iterator iterlogin = loginlist.begin();
@@ -199,8 +201,8 @@ unsigned WINAPI EchoThreadMain(LPVOID pComPort) {
 				ReleaseMutex(hMutex);
 
 				closesocket(sock);
-				delete(handleInfo);
-				delete(ioInfo);
+				free(handleInfo);
+				free(ioInfo);
 				continue;
 			}
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
@@ -208,98 +210,70 @@ unsigned WINAPI EchoThreadMain(LPVOID pComPort) {
 			ioInfo->rwMode = WRITE;
 			//Flag check
 			sFlag flag;
-
+			
 			memcpy(&flag.flag, ioInfo->buffer, sizeof(flag.flag));
 			if (flag.flag == em_USER_LOGIN) {
 				//유저 리스트 추가
 				vLogin(sock, ioInfo->buffer);
-			
 			}
 			else if (flag.flag == em_USER_MATCHING) {
 				vMatching(sock);
-				
 			}
 			else if (flag.flag == em_USER_TEAM) {
 				vTeam(sock);
-				
 			}
 			else if (flag.flag == em_USER_POSITION) {//위치 정보 전달
-				vMessage(sock, ioInfo->buffer);
+				vPosition(sock, ioInfo->buffer);
 			}
 			else if (flag.flag == em_USER_MESSAGE) {
-
+				list<sMatching>::iterator iter = matchinglist.begin();
+				while (iter != matchinglist.end()) {
+					if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {//채팅창
+						sMessage sMsg;
+						memcpy(&sMsg, &ioInfo->buffer, sizeof(sMsg));
+						break;
+					}
+					iter++;
+				}
 			}
 			else if (flag.flag == em_USER_RAYSHOT) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vRayshot(sock, ioInfo->buffer);
 			}
 			else if (flag.flag == em_USER_CAMERA) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vCamera(sock, ioInfo->buffer);
 			}//카메라 각도 전송
 			else if (flag.flag == em_USER_FKEY) {
-				vMessage(sock, ioInfo->buffer);
+				vFkey(sock, ioInfo->buffer);
 			}//Fkey에 따른 정보 전송
 			else if (flag.flag == em_USER_ZKEY) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vZkey(sock, ioInfo->buffer);
 			}
 			else if (flag.flag == em_USER_XKEY) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vXkey(sock, ioInfo->buffer);
 				//Xkey에 따른 정보 전송
 			}
 			else if (flag.flag == em_USER_CKEY) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vCkey(sock, ioInfo->buffer);
 			}
 			else if (flag.flag == em_USER_VKEY) {
-				vMessage(sock, ioInfo->buffer);
-				
-			}
-			else if (flag.flag == em_USER_RKEY) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vVkey(sock, ioInfo->buffer);
 			}
 			else if (flag.flag == em_USER_GAMEDEATH) {
-				vMessage(sock, ioInfo->buffer);
-				
-			}
-			else if (flag.flag == em_USER_RESOURCE) {
-				vMessage(sock, ioInfo->buffer);
-				
-			}
-			else if (flag.flag == em_USER_STATUS) {
-				vMessage(sock, ioInfo->buffer);
-				
-			}
-			else if (flag.flag == em_OBJECT_TREE) {
-				vMessage(sock, ioInfo->buffer);
-				
-			}
-			else if (flag.flag == em_OBJECT_STONE) {
-				vMessage(sock, ioInfo->buffer);
-				
-			}
-			else if (flag.flag == em_OBJECT_BULLET) {
-				vMessage(sock, ioInfo->buffer);
-				
+				vGamedeath(sock, ioInfo->buffer);
 			}
 			//WSASend(sock, &(ioInfo->wsabuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-			
-			//delete(ioInfo);
+
 			//데이터 받기
-			//ioInfo = new PER_IO_DATA;
+			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 			ioInfo->wsabuf.len = BUF_SIZE;
 			ioInfo->wsabuf.buf = ioInfo->buffer;
 			ioInfo->rwMode = READ;
 			WSARecv(sock, &(ioInfo->wsabuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
-			
 		}
 		else {
 			puts("message sent!");
-			//delete(ioInfo);
+			free(ioInfo);
 		}
 	}
 	return 0;
@@ -312,14 +286,13 @@ void ErrorHandling(const char *message) {
 }
 
 void c_send(SOCKET UserSocket, char* buffer) {
-	LPPER_IO_DATA ioInfo = new PER_IO_DATA;
+	LPPER_IO_DATA ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 	memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 	ioInfo->wsabuf.len = BUF_SIZE;
 	ioInfo->wsabuf.buf = ioInfo->buffer;
 	ioInfo->rwMode = WRITE;
 	memcpy(ioInfo->buffer, buffer, sizeof(ioInfo->buffer));
 	WSASend(UserSocket, &(ioInfo->wsabuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
-	delete(ioInfo);
 }
 void vMatching(SOCKET sock) {
 	sMatching match;
@@ -340,7 +313,7 @@ void vMatching(SOCKET sock) {
 				iter->UserSocket[1] = sock;
 				c_send(iter->UserSocket[1], (char*)&match);
 				for (int a = 0; a < 2; a++)
-					c_send(iter->UserSocket[a], (char*)&match);
+						c_send(iter->UserSocket[a], (char*)&match);
 				break;
 			}
 			else if (iter->UserSocket[2] == 0) {
@@ -348,7 +321,7 @@ void vMatching(SOCKET sock) {
 				for (int b = 0; b < 2; b++)
 					c_send(iter->UserSocket[2], (char*)&match);
 				for (int a = 0; a < 3; a++)
-					c_send(iter->UserSocket[a], (char*)&match);
+						c_send(iter->UserSocket[a], (char*)&match);
 				break;
 			}
 			else if (iter->UserSocket[3] == 0) {
@@ -356,17 +329,19 @@ void vMatching(SOCKET sock) {
 				for (int b = 0; b < 3; b++)
 					c_send(iter->UserSocket[3], (char*)&match);
 				for (int a = 0; a < 4; a++)
-					c_send(iter->UserSocket[a], (char*)&match);
+						c_send(iter->UserSocket[a], (char*)&match);
 				break;
 			}
 			iter++;
 			if (iter == matchinglist.end()) {
+				sMatching match;
 				match.UserSocket[0] = sock;
 				matchinglist.push_back(match);
 				break;
 			}
 		}
 	}
+
 }
 void vTeam(SOCKET sock) {
 	list<sMatching>::iterator iter = matchinglist.begin();
@@ -378,6 +353,36 @@ void vTeam(SOCKET sock) {
 				c_send(iter->UserSocket[a], (char*)&pos);
 			}
 			break;
+		}
+		iter++;
+	}
+}
+void vPosition(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
+	}
+}
+void vRayshot(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
 		}
 		iter++;
 	}
@@ -410,20 +415,124 @@ void vLogin(SOCKET sock, char* buffer) {
 		}
 	}
 }
-void vMessage(SOCKET sock, char* buffer) {
-	list<sMatching>::iterator miter = matchinglist.begin();
-	while (miter != matchinglist.end()) {
-		if (miter->UserSocket[0] == sock || miter->UserSocket[1] == sock || miter->UserSocket[2] == sock || miter->UserSocket[3] == sock) {
-
-			for (a = 0; a < 4; a++) {
-				if (miter->UserSocket[a] == sock)
+void vStructure_wall(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
 					continue;
 				else {
-					c_send(miter->UserSocket[a], buffer);
+					c_send(iter->UserSocket[a], buffer);
+				}
+
+			}
+		}
+		iter++;
+	}
+}
+void vCamera(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
 				}
 			}
-			break;
 		}
-		miter++;
+		iter++;
+	}
+}
+void vFkey(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
+	}
+}
+void vZkey(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
+	}
+}
+void vXkey(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
+	}
+}
+void vCkey(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
+	}
+}
+void vVkey(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
+	}
+}
+void vGamedeath(SOCKET sock, char* buffer) {
+	list<sMatching>::iterator iter = matchinglist.begin();
+	while (iter != matchinglist.end()) {
+		if (iter->UserSocket[0] == sock || iter->UserSocket[1] == sock || iter->UserSocket[2] == sock || iter->UserSocket[3] == sock) {
+			for (int a = 0; a < 4; a++) {
+				if (iter->UserSocket[a] == sock)
+					continue;
+				else {
+					c_send(iter->UserSocket[a], buffer);
+				}
+			}
+		}
+		iter++;
 	}
 }
